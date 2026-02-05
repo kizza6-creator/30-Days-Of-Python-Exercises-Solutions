d0ddbf3efbdffc44793ba169d89f28de54b73fd8
Kizza <kierennobrega@gmail.com>
2026-02-05 13:39:18 +1100

    lvl 1 exercises for Day 02.

---
+++ 02 - Variables and Builtin Functions/lvl1-exercises.py
@@ -0,0 +1,46 @@
+# Write a python comment saying 'Day 2: 30 Days of python programming'
+# 'Day 2: 30 Days of python programming'
+
+# Declare a first name variable and assign a value to it
+first_name = 'Kizza'
+print(first_name)
+
+# Declare a last name variable and assign a value to it
+last_name = 'Smith'
+print(last_name)
+
+# Declare a full name variable and assign a value to it.
+full_name = f"{first_name}, {last_name}"
+print(full_name)
+
+# Declare a country variable and assign a value to it
+country = 'Portugal'
+print(country)
+
+# Declare a city variable and assign a value to it
+city = 'Sydney'
+print(city)
+
+# Declare an age variable and assign a value to it
+age = 25
+print(age)
+
+# Declare a year variable and assign a value to it
+year = 2026
+print(year)
+
+# Declare a variable is_married and assign a value to it
+is_married = False
+print(is_married)
+
+# Declare a variable is_true and assign a value to it
+is_true = True
+print(is_true)
+
+# Declare a variable is_light_on and assign a value to it
+is_light_on = False
+print(is_light_on)
+
+# Declare multiple variable on one line
+firstname, lastname, age = 'Kieren', 'Smith', 25
+print(first_name, last_name, age)
